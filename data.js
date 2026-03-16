298a11711c7ef1ee75bd56d8018967a3b726462c
Van9a <matyushko.vanya.195@gmail.com>
2026-03-16 21:06:33 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,22 @@
+const krokData = {
+    subjects: [
+        {
+            id: "pharmacology",
+            title: "Фармакологія",
+            titleEn: "Pharmacology",
+            topics: [
+                { id: "ph-1", title: "Загальна фармакологія", content: "Конспект тут...", questions: [...] },
+                { id: "ph-2", title: "Вегетативна нервова система", content: "...", questions: [] }
+            ]
+        },
+        {
+            id: "pathomorphology",
+            title: "Патоморфологія",
+            titleEn: "Pathomorphology",
+            topics: [
+                { id: "pm-1", title: "Пошкодження клітин та некроз", content: "...", questions: [] }
+            ]
+        }
+        // Додаємо інші 4 предмети аналогічно
+    ]
+};
