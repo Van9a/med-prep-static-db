a57714bf1fac29a41eca595aed56b32f0c4425b8
Van9a <matyushko.vanya.195@gmail.com>
2026-03-16 21:09:04 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,22 +1,13 @@
-const krokData = {
+const medDB = {
     subjects: [
-        {
-            id: "pharmacology",
-            title: "Фармакологія",
-            titleEn: "Pharmacology",
-            topics: [
-                { id: "ph-1", title: "Загальна фармакологія", content: "Конспект тут...", questions: [...] },
-                { id: "ph-2", title: "Вегетативна нервова система", content: "...", questions: [] }
-            ]
-        },
-        {
-            id: "pathomorphology",
-            title: "Патоморфологія",
-            titleEn: "Pathomorphology",
-            topics: [
-                { id: "pm-1", title: "Пошкодження клітин та некроз", content: "...", questions: [] }
-            ]
-        }
-        // Додаємо інші 4 предмети аналогічно
+        { id: "pharma", title: "Фармакологія", en: "Pharmacology", topics: [
+            { id: "ph-1", title: "Загальна фармакологія", content: "Конспект по загальній фармі...", questions: [] },
+            { id: "ph-2", title: "Вегетативна нервова система", content: "...", questions: [] }
+        ]},
+        { id: "pathmorph", title: "Патоморфологія", en: "Pathomorphology", topics: [] },
+        { id: "pathphys", title: "Патофізіологія", en: "Pathophysiology", topics: [] },
+        { id: "anatomy", title: "Анатомія", en: "Anatomy", topics: [] },
+        { id: "physio", title: "Фізіологія", en: "Physiology", topics: [] },
+        { id: "histo", title: "Гістологія", en: "Histology", topics: [] }
     ]
 };
